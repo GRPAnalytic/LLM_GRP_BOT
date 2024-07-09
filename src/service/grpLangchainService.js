807f31dca0054ed19d04e251b52fc5ad4601e84e
apta-marcel <marcel@pervasif.com>
2024-07-09 09:07:31 +0700

    Change llm api endpoint

--- src/service/grpLangchainService.js
+++ src/service/grpLangchainService.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 const postQuestion = async (message, context = "", includeTables = []) => {
-    const res = await fetch("http://172.16.30.170:8000/answer/", {
+    const res = await fetch("http://13.82.183.135:8000/answer/", {
         body: JSON.stringify({
             context,
             question: message,
